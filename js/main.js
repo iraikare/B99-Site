0b2a37b30d21f1625fb9bb8e728ef390a4cbfbe4
iraikare <101957823+iraikare@users.noreply.github.com>
2023-05-11 04:09:57 -0300

    Add files via upload
    upload site b99

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function abrir(){
+    document.getElementById('esmaecer').style.visibility = 'visible';
+    document.getElementById('menu').style.visibility = 'visible';
+    document.getElementById('x').style.visibility = 'visible';
+    document.getElementById('sanduiche').style.visibility = 'hidden';
+}
+function fechar(){
+    document.getElementById('esmaecer').style.visibility = 'hidden';
+    document.getElementById('menu').style.visibility = 'hidden';
+    document.getElementById('x').style.visibility = 'hidden';
+    document.getElementById('sanduiche').style.visibility = 'visible';
+}
